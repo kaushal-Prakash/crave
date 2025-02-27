73fb3f6f8c8ad591509420026637f3ec05db24c2
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-02-27 19:29:04 +0530

    navbar ui updates

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,7 +11,6 @@ function Navbar() {
       {/* Logo */}
       <div className="text-2xl font-bold text-appetizingRed text-amber-950 tracking-widest">Crave</div>
 
-      {/* Menu Icon (always visible) */}
       <button
         onClick={() => setIsOpen(true)}
         className="text-3xl text-gray-800 cursor-pointer"
@@ -19,13 +18,11 @@ function Navbar() {
         <FiMenu />
       </button>
 
-      {/* Full-screen Overlay Menu */}
       <div
         className={`fixed top-0 left-0 w-full bg-gradient-to-br from-cyan-200 to-orange-400 flex flex-col items-center justify-center gap-8 text-2xl transition-transform duration-300 h-screen font-semibold tracking-wide text-amber-800 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        {/* Close Button */}
         <button
           onClick={() => setIsOpen(false)}
           className="absolute top-5 right-5 text-4xl cursor-pointer text-gray-800"
@@ -37,28 +34,28 @@ function Navbar() {
         <Link
           href="/home"
           onClick={() => setIsOpen(false)}
-          className="text-gray-800 hover:text-appetizingRed transition"
+          className="text-gray-800 hover:text-appetizingRed hover:scale-110 hover:underline transition-all duration-100"
         >
           Home
         </Link>
         <Link
           href="/login"
           onClick={() => setIsOpen(false)}
-          className="text-gray-800 hover:text-appetizingRed transition"
+          className="text-gray-800 hover:text-appetizingRed hover:scale-110 hover:underline transition-all duration-100"
         >
           Login
         </Link>
         <Link
           href="/signup"
           onClick={() => setIsOpen(false)}
-          className="text-gray-800 hover:text-appetizingRed transition"
+          className="text-gray-800 hover:text-appetizingRed hover:scale-110 hover:underline transition-all duration-100"
         >
           signup
         </Link>
         <Link
           href="/about-us"
           onClick={() => setIsOpen(false)}
-          className="text-gray-800 hover:text-appetizingRed transition"
+          className="text-gray-800 hover:text-appetizingRed hover:scale-110 hover:underline transition-all duration-100"
         >
           About Us
         </Link>
