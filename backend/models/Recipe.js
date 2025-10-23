6c2022164b7517d87aa54b78a1fcf646b3fb60c2
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-10-23 18:51:27 +0530

    add comments to Recipe.js

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -7,7 +7,8 @@ export const createRecipe = async (title, description, user_id) => {
       "INSERT INTO recipes (title, description, user_id) VALUES (?, ?, ?)",
       [title, description, user_id]
     );
-    return result.insertId;
+    // The insertId is a property on the result object returned by the connection.execute() method after an INSERT statement has successfully executed.
+    return result.insertId; // <-- This returns the new recipe's ID
   } catch (error) {
     throw error;
   }
@@ -16,6 +17,7 @@ export const createRecipe = async (title, description, user_id) => {
 export const getAllRecipes = async () => {
   try {
     const connection = await connectDB();
+    // The syntax const [rows] is a JavaScript way to extract the first element of that returned array and name it rows. So u can change it to other names too.
     const [rows] = await connection.execute("SELECT * FROM recipes order by id desc");
     return rows;
   } catch (error) {
