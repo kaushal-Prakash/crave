2523d8cebcc567b5c8f6686a281d5c69a7be8c94
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2026-01-07 00:39:53 +0530

    fixed font import error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 const poppins = Poppins({
-  variable: "--poppins",
+  variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
