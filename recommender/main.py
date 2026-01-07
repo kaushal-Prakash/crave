f6cc35f87ce2563ee3959f3404ccde9240f9d84a
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2026-01-07 10:31:24 +0530

    added cors

--- recommender/main.py
+++ recommender/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from recommender import get_similar_recipes
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+# Allow your frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",   # Next.js 
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
